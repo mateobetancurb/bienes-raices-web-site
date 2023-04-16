0d624004e45d263c922095c56af5b07ce0e0fcf9
Mateo <mateobetancurb@gmail.com>
2023-04-16 12:44:11 -0500

    configuracion de variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,19 +1,27 @@
 const sequelize = require("sequelize");
+const dotenv = require("dotenv");
 
-const db = new sequelize("bienes_raices_app", "root", "root", {
-	host: "localhost",
-	port: 3306,
-	dialect: "mysql",
-	define: {
-		timestamps: true,
-	},
-	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000,
-	},
-	operatorAliases: false,
-});
+dotenv.config({ path: ".env" });
+
+const db = new sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASSWORD,
+	{
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		dialect: "mysql",
+		define: {
+			timestamps: true,
+		},
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+		operatorAliases: false,
+	}
+);
 
 module.exports = db;
