55b08898070971cbcb1e339a6bc73a5aec052c44
Mateo <mateobetancurb@gmail.com>
2023-04-16 11:46:20 -0500

    rutas de autenticacion

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-
 const router = express.Router();
+const {
+	formularioLogin,
+	formularioRegistro,
+	formularioOlvidePassword,
+} = require("../controllers/usuarioController");
 
 //routing
-router.get("/login");
+router.get("/login", formularioLogin);
+router.get("/registro", formularioRegistro);
+router.get("/olvide-password", formularioOlvidePassword);
 
 module.exports = router;
