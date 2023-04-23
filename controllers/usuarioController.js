10bb9612919531eaff4cd9ee5eee33ea6e49ebef
Mateo <mateobetancurb@gmail.com>
2023-04-23 11:33:02 -0500

    controller nuevo usuario y validacion formularios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,6 @@
+const { check, validationResult } = require("express-validator");
+const Usuario = require("../models/Usuario.js");
+
 const formularioLogin = (req, res) => {
 	res.render("auth/login", {
 		pagina: "Iniciar sesión",
@@ -10,6 +13,40 @@ const formularioRegistro = (req, res) => {
 	});
 };
 
+const registrar = async (req, res) => {
+	await check("nombre")
+		.notEmpty()
+		.withMessage("El nombre es obligatorio")
+		.run(req);
+
+	await check("email")
+		.isEmail()
+		.withMessage("Escribe un correo válido")
+		.run(req);
+
+	await check("password")
+		.isLength({ min: 6 })
+		.withMessage("La contraseña debe más de 5 caracteres")
+		.run(req);
+
+	await check("password")
+		.equals("password")
+		.withMessage("Las contraseñas deben ser iguales")
+		.run(req);
+
+	let resultado = validationResult(req);
+
+	if (!resultado.isEmpty()) {
+		return res.render("auth/registro", {
+			pagina: "Crear cuenta",
+			errores: resultado.array(),
+		});
+	}
+
+	const usuario = await Usuario.create(req.body);
+	res.json(usuario);
+};
+
 const formularioOlvidePassword = (req, res) => {
 	res.render("auth/olvide-password", {
 		pagina: "Recupera tu acceso a Bienes Raíces",
@@ -19,5 +56,6 @@ const formularioOlvidePassword = (req, res) => {
 module.exports = {
 	formularioLogin,
 	formularioRegistro,
+	registrar,
 	formularioOlvidePassword,
 };
