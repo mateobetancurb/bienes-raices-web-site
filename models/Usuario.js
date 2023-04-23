60277abe05bef3200d75cee8f96ae01d7437ed36
Mateo <mateobetancurb@gmail.com>
2023-04-23 13:46:00 -0500

    hashear password antes de crear usuario

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,22 +1,34 @@
 const { DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 
 const db = require("../config/db.js");
 
-const Usuario = db.define("usuarios", {
-	nombre: {
-		type: DataTypes.STRING,
-		allowNull: false,
+const Usuario = db.define(
+	"usuarios",
+	{
+		nombre: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		token: DataTypes.STRING,
+		confirmado: DataTypes.BOOLEAN,
 	},
-	email: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	password: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	token: DataTypes.STRING,
-	confirmado: DataTypes.BOOLEAN,
-});
+	{
+		hooks: {
+			beforeCreate: async function (usuario) {
+				const salt = await bcrypt.genSalt(10);
+				usuario.password = await bcrypt.hash(usuario.password, salt);
+			},
+		},
+	}
+);
 
 module.exports = Usuario;
