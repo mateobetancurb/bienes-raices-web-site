76eb9a9d34be7b35baa342bed5b66584f926f12b
Mateo <mateobetancurb@gmail.com>
2023-04-23 11:32:21 -0500

    ruta para crear un usuario

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
 	formularioLogin,
 	formularioRegistro,
+	registrar,
 	formularioOlvidePassword,
 } = require("../controllers/usuarioController");
 
 //routing
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
+router.post("/registro", registrar);
 router.get("/olvide-password", formularioOlvidePassword);
 
 module.exports = router;
