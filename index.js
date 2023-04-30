faff98b477fe97bc81c9cab47fc865194a570b3f
Mateo <mateobetancurb@gmail.com>
2023-04-30 16:09:55 -0500

    configuracion puerto servidor en variable de entorno

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.static("public"));
 app.use("/auth", usuarioRoutes);
 
 //definir un puerto y arrancar
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`Server runing in port ${port}`);
