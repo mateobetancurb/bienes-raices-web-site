9af83f1ff0bf8dfc7b619b09124723d99f64e232
Mateo <mateobetancurb@gmail.com>
2023-05-01 18:03:57 -0500

    rutas para restablecer contraseña

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const router = express.Router();
+
 const {
 	formularioLogin,
 	formularioRegistro,
 	registrar,
 	confirmarCuenta,
 	formularioOlvidePassword,
+	resetearPassword,
+	comprobarToken,
+	nuevoPassword,
 } = require("../controllers/usuarioController");
 
-//routing
 router.get("/login", formularioLogin);
 router.get("/registro", formularioRegistro);
 router.post("/registro", registrar);
 router.get("/confirmar/:token", confirmarCuenta);
 router.get("/olvide-password", formularioOlvidePassword);
+router.post("/olvide-password", resetearPassword);
+router.get("/olvide-password/:token", comprobarToken);
+router.post("/olvide-password/:token", nuevoPassword);
 
 module.exports = router;
