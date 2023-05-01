fd62202a5c525a7e9d05ac2acc88b07d76699e19
Mateo <mateobetancurb@gmail.com>
2023-05-01 18:02:26 -0500

    email para resetear contraseña

--- helpers/emails.js
+++ helpers/emails.js
@@ -29,6 +29,36 @@ const emailRegistro = async (datos) => {
 	});
 };
 
+const emailResetearPassword = async (datos) => {
+	const transport = nodemailer.createTransport({
+		host: process.env.EMAIL_HOST,
+		port: process.env.EMAIL_PORT,
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASSWORD,
+		},
+	});
+
+	const { email, nombre, token } = datos;
+
+	//enviar el email
+	await transport.sendMail({
+		from: "Bienes Raices App",
+		to: email,
+		subject: "Restablece tu contraseña en BienesRaices",
+		text: "Restablece tu contraseña en BienesRaices",
+		html: `
+      <h2>Hola ${nombre}</h2>
+      <p>Nos has pedido cambiar tu contraseña. Dale clic al link para generar una nueva contraseña</p>
+      <a href="${process.env.BACKEND_URL}:${
+			process.env.PORT ?? 3000
+		}/auth/olvide-password/${token}">Restablecer contraseña</a>
+      <h5>Si no quieres cambiar tu contraseña puedes ignorar este mensaje</h5>
+    `,
+	});
+};
+
 module.exports = {
 	emailRegistro,
+	emailResetearPassword,
 };
