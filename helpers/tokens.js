cbab36618ece13a57d4b45395ccb2aaa6b1e15f1
Mateo <mateobetancurb@gmail.com>
2023-05-02 12:59:06 -0500

    helper para generar JWT

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -1,7 +1,18 @@
+const JWT = require("jsonwebtoken");
+
+const generarJWT = (datos) => {
+	return JWT.sign(
+		{ id: datos.id, nombre: datos.nombre },
+		process.env.JWT_SECRET,
+		{ expiresIn: "8h" }
+	);
+};
+
 const generarId = () => {
 	return Math.random().toString(32).substring(2) + Date.now().toString(32);
 };
 
 module.exports = {
+	generarJWT,
 	generarId,
 };
