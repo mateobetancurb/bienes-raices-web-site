32bdced6d871447ebfe2c90e657a198f699629ae
Mateo <mateobetancurb@gmail.com>
2023-05-02 11:31:17 -0500

    funcion para validar contraseña en el login

--- models/Usuario.js
+++ models/Usuario.js
@@ -31,4 +31,8 @@ const Usuario = db.define(
 	}
 );
 
+Usuario.prototype.verificarPassword = function (password) {
+	return bcrypt.compareSync(password, this.password);
+};
+
 module.exports = Usuario;
