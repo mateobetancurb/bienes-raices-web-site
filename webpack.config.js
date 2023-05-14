0f8b4c0edcfbdcea5d07f309c388ddaa7d549b50
Mateo <mateobetancurb@gmail.com>
2023-05-14 14:23:23 -0500

    archivo configuracion de webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    mapa:"./src/js/mapa.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("public/js")
+
+  }
+};
