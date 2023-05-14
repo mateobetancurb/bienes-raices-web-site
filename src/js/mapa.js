3422b1982c9f0dfb6b3610f30b7239510687e422
Mateo <mateobetancurb@gmail.com>
2023-05-14 18:25:13 -0500

    obteniendo direccion con base en el movimiento pin

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -2,9 +2,42 @@
 	const lat = 6.2476;
 	const lng = -75.5658;
 	const mapa = L.map("mapa").setView([lat, lng], 14);
+	let marker;
+
+	//utilizar el provider y el geocoder
+	const geocodeService = L.esri.Geocoding.geocodeService();
 
 	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		attribution:
 			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	}).addTo(mapa);
+
+	//ubicacion del pin
+	marker = new L.marker([lat, lng], {
+		draggable: true,
+		autoPan: true,
+	}).addTo(mapa);
+
+	//detectar movimiento del pin
+	marker.on("moveend", function (e) {
+		marker = e.target;
+		const posicion = marker.getLatLng();
+		mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));
+
+		//obtener la informacion de las calles al soltar el pin
+		geocodeService
+			.reverse()
+			.latlng(posicion, 14)
+			.run(function (error, resultado) {
+				marker.bindPopup(resultado.address.LongLabel);
+
+				//llenar los campos
+				document.querySelector(".calle").textContent =
+					resultado?.address?.Address ?? "";
+				document.getElementById("calle").value =
+					resultado?.address?.Address ?? "";
+				document.getElementById("lat").value = resultado?.latlng?.lat ?? "";
+				document.getElementById("lng").value = resultado?.latlng?.lng ?? "";
+			});
+	});
 })();
