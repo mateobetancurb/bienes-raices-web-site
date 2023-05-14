cd976bb690fadd48bb217bfbb0b786cd6cd3090c
Mateo <mateobetancurb@gmail.com>
2023-05-14 14:24:08 -0500

    ruta para crear propiedades

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { admin } = require("../controllers/propiedadesController.js");
+const {
+	admin,
+	crearPropiedad,
+} = require("../controllers/propiedadesController.js");
 
 router.get("/mis-propiedades", admin);
+router.get("/propiedades/crear", crearPropiedad);
 
 module.exports = router;
