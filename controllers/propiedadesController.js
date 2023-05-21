50cc0361d0ed337cd683c50d817b9f70b3fcc288
Mateo <mateobetancurb@gmail.com>
2023-05-21 17:33:33 -0500

    renderizando categorias y precios

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -1,3 +1,6 @@
+const Precio = require("../models/Precio.js");
+const Categoria = require("../models/Categoria.js");
+
 const admin = (req, res) => {
 	res.render("propiedades/admin", {
 		pagina: "Mis propiedades",
@@ -5,11 +8,19 @@ const admin = (req, res) => {
 	});
 };
 
-const crearPropiedad = (req, res) => {
-  	res.render("propiedades/crear", {
-			pagina: "Mis propiedades",
-			barra: true,
-		});
+const crearPropiedad = async (req, res) => {
+  //consultar modelo de precios y categorias
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll(),
+  ]);
+
+	res.render("propiedades/crear", {
+		pagina: "Mis propiedades",
+    barra: true,
+    categorias,
+    precios,
+	});
 };
 
 module.exports = {
