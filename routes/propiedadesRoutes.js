e4fe4b3f31ca594ee26bdfe3319c6118f9b48583
Mateo <mateobetancurb@gmail.com>
2023-05-21 19:04:29 -0500

    validaciones en el router

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const { body } = require("express-validator");
 const router = express.Router();
 const {
 	admin,
 	crearPropiedad,
+	guardarPropiedad,
 } = require("../controllers/propiedadesController.js");
 
 router.get("/mis-propiedades", admin);
 router.get("/propiedades/crear", crearPropiedad);
+router.post(
+	"/propiedades/crear",
+  body("titulo").notEmpty().withMessage("Ingresa un título"),
+  body("precio").notEmpty().withMessage("Ingresa un precio"),
+	guardarPropiedad
+);
 
 module.exports = router;
