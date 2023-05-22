1ca084eb825a25424fe03adb86a280806a76d994
Mateo <mateobetancurb@gmail.com>
2023-05-22 13:11:14 -0500

    middleware en las rutas

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const { body } = require("express-validator");
 const router = express.Router();
 const {
 	admin,
 	crearPropiedad,
 	guardarPropiedad,
 } = require("../controllers/propiedadesController.js");
+const protegerRuta = require("../middlewares/protegerRuta.js");
 
-router.get("/mis-propiedades", admin);
-router.get("/propiedades/crear", crearPropiedad);
-router.post("/propiedades/crear", guardarPropiedad);
+router.get("/mis-propiedades", protegerRuta, admin);
+router.get("/propiedades/crear", protegerRuta, crearPropiedad);
+router.post("/propiedades/crear", protegerRuta, guardarPropiedad);
 
 module.exports = router;
