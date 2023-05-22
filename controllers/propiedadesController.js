785e9a15958518b63f2b980193f88d82c3ec6373
Mateo <mateobetancurb@gmail.com>
2023-05-22 11:35:54 -0500

    validando el formulario

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -1,4 +1,4 @@
-const { validationResult } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 const Precio = require("../models/Precio.js");
 const Categoria = require("../models/Categoria.js");
 
@@ -21,11 +21,44 @@ const crearPropiedad = async (req, res) => {
 		barra: true,
 		categorias,
 		precios,
+		datos: {},
 	});
 };
 
 const guardarPropiedad = async (req, res) => {
 	//validar formulario
+	await check("titulo").notEmpty().withMessage("Ingresa un título").run(req);
+	await check("descripcion")
+		.notEmpty()
+		.withMessage("La descripción no puede ir vacía")
+		.isLength({ max: 200 })
+		.withMessage("La descripción no puede tener más de 200 caracteres")
+		.run(req);
+	await check("categoria")
+		.isNumeric()
+		.withMessage("Selecciona una categoría")
+		.run(req);
+	await check("precio")
+		.isNumeric()
+		.withMessage("Selecciona un rango de precios")
+		.run(req);
+	await check("habitaciones")
+		.isNumeric()
+		.withMessage("Selecciona la cantidad de habitaciones")
+		.run(req);
+	await check("estacionamientos")
+		.isNumeric()
+		.withMessage("Selecciona la cantidad de parqueaderos")
+		.run(req);
+	await check("banos")
+		.isNumeric()
+		.withMessage("Selecciona la cantidad de baños")
+		.run(req);
+	await check("lat")
+		.notEmpty()
+		.withMessage("Ubica la propiedad en el mapa")
+		.run(req);
+
 	let resultado = validationResult(req);
 
 	if (!resultado.isEmpty()) {
@@ -36,10 +69,11 @@ const guardarPropiedad = async (req, res) => {
 
 		return res.render("propiedades/crear", {
 			pagina: "Crear propiedad",
-      barra: true,
-      categorias,
-      precios,
+			barra: true,
+			categorias,
+			precios,
 			errores: resultado.array(),
+			datos: req.body,
 		});
 	}
 };
