59b480abd9ec4b652645c2288ead4b21d35275f8
Mateo <mateobetancurb@gmail.com>
2023-05-22 11:35:31 -0500

    ruta crear propiedad

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -9,11 +9,6 @@ const {
 
 router.get("/mis-propiedades", admin);
 router.get("/propiedades/crear", crearPropiedad);
-router.post(
-	"/propiedades/crear",
-  body("titulo").notEmpty().withMessage("Ingresa un título"),
-  body("precio").notEmpty().withMessage("Ingresa un precio"),
-	guardarPropiedad
-);
+router.post("/propiedades/crear", guardarPropiedad);
 
 module.exports = router;
