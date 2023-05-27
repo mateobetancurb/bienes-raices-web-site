0ea3ae33370a1a4e652fe9fabe4e4428686719a7
Mateo <mateobetancurb@gmail.com>
2023-05-27 16:50:11 -0500

    rutas para subir imagenes - dropzone

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -4,11 +4,13 @@ const {
 	admin,
 	crearPropiedad,
 	guardarPropiedad,
+	agregarImagen,
 } = require("../controllers/propiedadesController.js");
 const protegerRuta = require("../middlewares/protegerRuta.js");
 
 router.get("/mis-propiedades", protegerRuta, admin);
 router.get("/propiedades/crear", protegerRuta, crearPropiedad);
 router.post("/propiedades/crear", protegerRuta, guardarPropiedad);
+router.get("/propiedades/agregar-imagen/:id", protegerRuta, agregarImagen);
 
 module.exports = router;
