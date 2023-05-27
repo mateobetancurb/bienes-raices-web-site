8dc98de7c6db0109d6b519c3225c295f6448ac0d
Mateo <mateobetancurb@gmail.com>
2023-05-27 16:48:06 -0500

    configurando dropzone en webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    mapa:"./src/js/mapa.js"
+    mapa: "./src/js/mapa.js",
+    agregarImagen: "./src/js/agregarImagen.js",
   },
   output: {
     filename: "[name].js",
