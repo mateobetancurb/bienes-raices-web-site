8839e078740ebaed74a7b53d9ad3921820140762
Mateo <mateobetancurb@gmail.com>
2023-05-27 18:35:32 -0500

    ruta almacenar imagen db

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -5,12 +5,20 @@ const {
 	crearPropiedad,
 	guardarPropiedad,
 	agregarImagen,
+	almacenarImagen,
 } = require("../controllers/propiedadesController.js");
 const protegerRuta = require("../middlewares/protegerRuta.js");
+const upload = require("../middlewares/subirImagen.js");
 
 router.get("/mis-propiedades", protegerRuta, admin);
 router.get("/propiedades/crear", protegerRuta, crearPropiedad);
 router.post("/propiedades/crear", protegerRuta, guardarPropiedad);
 router.get("/propiedades/agregar-imagen/:id", protegerRuta, agregarImagen);
+router.post(
+	"/propiedades/agregar-imagen/:id",
+	protegerRuta,
+	upload.single("imagen"),
+	almacenarImagen
+);
 
 module.exports = router;
