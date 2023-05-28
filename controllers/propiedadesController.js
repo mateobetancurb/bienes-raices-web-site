c1831fd3a53dfef12cefca2f57c0f4540bb50e5d
Mateo <mateobetancurb@gmail.com>
2023-05-28 13:26:19 -0500

    controller editar propiedad

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -163,10 +163,122 @@ const almacenarImagen = async (req, res) => {
 	}
 };
 
+const editarPropiedad = async (req, res) => {
+	//validar que la propiedad exista
+	const propiedad = await Propiedad.findByPk(req.params.id);
+
+	if (!propiedad) {
+		return res.redirect("/mis-propiedades");
+	}
+
+	//propiedad pertenece al usuario
+	if (propiedad.usuarioId !== req.usuario.id) {
+		return res.redirect("/mis-propiedades");
+	}
+
+	//consultar modelo de precios y categorias
+	const [categorias, precios] = await Promise.all([
+		Categoria.findAll(),
+		Precio.findAll(),
+	]);
+
+	res.render("propiedades/editar", {
+		pagina: `Editar propiedad: ${propiedad.titulo}`,
+		categorias,
+		precios,
+		datos: propiedad,
+	});
+};
+
+const guardarPropiedadEditada = async (req, res) => {
+	//validar formulario
+	await check("titulo").notEmpty().withMessage("Ingresa un título").run(req);
+	await check("descripcion")
+		.notEmpty()
+		.withMessage("La descripción no puede ir vacía")
+		.isLength({ max: 200 })
+		.withMessage("La descripción no puede tener más de 200 caracteres")
+		.run(req);
+	await check("categoria")
+		.isNumeric()
+		.withMessage("Selecciona una categoría")
+		.run(req);
+	await check("precio")
+		.isNumeric()
+		.withMessage("Selecciona un rango de precios")
+		.run(req);
+	await check("habitaciones")
+		.isNumeric()
+		.withMessage("Selecciona la cantidad de habitaciones")
+		.run(req);
+	await check("estacionamiento")
+		.isNumeric()
+		.withMessage("Selecciona la cantidad de parqueaderos")
+		.run(req);
+	await check("banos")
+		.isNumeric()
+		.withMessage("Selecciona la cantidad de baños")
+		.run(req);
+	await check("lat")
+		.notEmpty()
+		.withMessage("Ubica la propiedad en el mapa")
+		.run(req);
+
+	let resultado = validationResult(req);
+
+	if (!resultado.isEmpty()) {
+		const [categorias, precios] = await Promise.all([
+			Categoria.findAll(),
+			Precio.findAll(),
+		]);
+
+		return res.render("propiedades/editar", {
+			pagina: "Editar propiedad",
+			categorias,
+			precios,
+			errores: resultado.array(),
+			datos: req.body,
+		});
+	}
+
+	//validar que la propiedad exista
+	const propiedad = await Propiedad.findByPk(req.params.id);
+
+	if (!propiedad) {
+		return res.redirect("/mis-propiedades");
+	}
+
+	//propiedad pertenece al usuario
+	if (propiedad.usuarioId !== req.usuario.id) {
+		return res.redirect("/mis-propiedades");
+	}
+
+	//actualizar la propiedad
+	try {
+		await propiedad.update({
+			titulo: req.body.titulo,
+			descripcion: req.body.descripcion,
+			habitaciones: req.body.habitaciones,
+			estacionamiento: req.body.estacionamiento,
+			banos: req.body.banos,
+			calle: req.body.calle,
+			lat: req.body.lat,
+			lng: req.body.lng,
+			precioId: req.body.precio,
+			categoriaId: req.body.categoria,
+		});
+		res.redirect("/mis-propiedades");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 module.exports = {
 	admin,
 	crearPropiedad,
 	guardarPropiedad,
 	agregarImagen,
 	almacenarImagen,
+	editarPropiedad,
+	guardarPropiedadEditada,
 };
