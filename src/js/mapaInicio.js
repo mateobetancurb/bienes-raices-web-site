bd6a2d281a252a88063ae134d61025a9a72046d2
Mateo <mateobetancurb@gmail.com>
2023-06-04 22:19:25 -0500

    filtrando propiedades

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -4,23 +4,46 @@
 	const mapa = L.map("mapa-inicio").setView([lat, lng], 14);
 
 	let markers = new L.FeatureGroup().addTo(mapa);
+	let propiedades = [];
+
+	//filtros
+	const filtros = {
+		categoria: "",
+		precio: "",
+	};
+
+	const categoriaSelect = document.getElementById("categoria");
+	const precioSelect = document.getElementById("precio");
 
 	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		attribution:
 			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	}).addTo(mapa);
 
+	//filtrado de categorias y precios
+	categoriaSelect.addEventListener("change", (e) => {
+		filtros.categoria = Number(e.target.value);
+		filtrarPropiedades();
+	});
+
+	precioSelect.addEventListener("change", (e) => {
+		filtros.precio = Number(e.target.value);
+		filtrarPropiedades();
+	});
+
 	const getPropiedades = async () => {
 		try {
 			const res = await fetch("/api/propiedades");
-			const propiedades = await res.json();
+			propiedades = await res.json();
 			mostrarPropiedades(propiedades);
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
-	const mostrarPropiedades = (propiedades) => {
+  const mostrarPropiedades = (propiedades) => {
+    //limpiar los markers
+
 		propiedades.forEach((propiedad) => {
 			//agregar los pines
 			const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
@@ -40,5 +63,29 @@
 			markers.addLayer(marker);
 		});
 	};
+
+	const filtrarPropiedades = () => {
+		const resultado = propiedades
+			.filter(filtrarCategoria)
+			.filter(filtrarPrecio);
+      mostrarPropiedades(resultado);
+	};
+
+	const filtrarCategoria = (propiedad) => {
+		if (filtros.categoria) {
+			return propiedad.categoriaId === filtros.categoria;
+		}
+    console.log(propiedad);
+		return propiedad;
+	};
+
+	const filtrarPrecio = (propiedad) => {
+		if (filtros.precio) {
+			return propiedad.precioId === filtros.precio;
+		}
+    console.log(propiedad);
+    return propiedad;
+	};
+
 	getPropiedades();
 })();
