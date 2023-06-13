e98a25e73f26cd0cb22615f445d7f125641c1386
Mateo <mateobetancurb@gmail.com>
2023-06-12 19:29:33 -0500

    config modelo mensaje y relaciones

--- models/index.js
+++ models/index.js
@@ -2,15 +2,21 @@ const Propiedad = require("./Propiedad.js");
 const Usuario = require("./Usuario.js");
 const Categoria = require("./Categoria.js");
 const Precio = require("./Precio.js");
+const Mensaje = require("./Mensaje.js");
 
 // Relaciones
 Propiedad.belongsTo(Precio, { foreignKey: "precioId" });
 Propiedad.belongsTo(Categoria, { foreignKey: "categoriaId" });
 Propiedad.belongsTo(Usuario, { foreignKey: "usuarioId" });
+Propiedad.hasMany(Mensaje, { foreignKey: "propiedadId" });
+
+Mensaje.belongsTo(Propiedad, { foreignKey: "propiedadId" });
+Mensaje.belongsTo(Usuario, { foreignKey: "usuarioId" });
 
 module.exports = {
-	Propiedad,
-	Usuario,
-	Categoria,
+  Propiedad,
 	Precio,
+	Categoria,
+	Usuario,
+	Mensaje,
 };
