bb2ea22a52d9e71e927e91d15683c42d3284863d
Mateo <mateobetancurb@gmail.com>
2023-06-19 10:52:47 -0500

    formatear fecha

--- helpers/index.js
+++ helpers/index.js
@@ -2,6 +2,17 @@ const esVendedor = (usuarioId, propiedadUsuarioId) => {
 	return usuarioId === propiedadUsuarioId;
 };
 
+const formatearFecha = (date) => {
+	const newDate = new Date(date);
+	const options = {
+		year: "numeric",
+		month: "short",
+		day: "numeric",
+	};
+	return newDate.toLocaleDateString("es-ES", options);
+};
+
 module.exports = {
 	esVendedor,
+	formatearFecha,
 };
